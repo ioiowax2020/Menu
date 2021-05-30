2592ae2e33105224dbc3c11a9ab1b9b2d891a1a1
Kou <ioiowax2020.jp@gmail.com>
2021-05-30 19:47:25 +0900

    feat: add user registeration

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const port = 3000
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 
@@ -14,7 +13,7 @@ const app = express()
 //setting engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 //setting static files
 app.use(express.static('public'))
 //methodoverrde 設定的app.use 要放在最接近路由的地方
