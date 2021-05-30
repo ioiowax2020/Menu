c83066baa76ee5ab66da16291c5e8ef93afa1ee1
Kou <ioiowax2020.jp@gmail.com>
2021-05-30 19:22:35 +0900

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,13 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+})
+
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 
 
