15029d1cca8435078f912f22f8cc92240bda512b
Kou <ioiowax2020.jp@gmail.com>
2021-05-30 20:08:57 +0900

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express')
+
 const port = 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
 
+const usePassport = require('./config/passport')
+
 const routes = require('./routes')
 require('./config/mongoose')
 
-
 const app = express()
+
+
 
 app.use(session({
   secret: 'ThisIsMymenuList',
   resave: false,
   saveUninitialized: true
-
-
 }))
 //setting engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -26,7 +28,7 @@ app.use(express.static('public'))
 //methodoverrde 設定的app.use 要放在最接近路由的地方
 app.use(methodOverride('_method'))
 
-
+usePassport(app)
 
 //將request 導入路由器
 app.use(routes)
